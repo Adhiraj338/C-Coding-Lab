f5b02e537f7e08c83096ae6c11eba71731da04e1
Adhiraj_1408 <adhirajramtirthe952@gmail.com>
2026-09-17 21:59:46 +0530

    Add files via upload
    Check number using if-else and NOT operator

---
+++ practice_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+
+int number;
+
+printf("Enter the number : \n");
+scanf("%d",&number);
+
+if(!(number == 10)){
+       printf("Number is 10\n");
+}
+
+else{
+printf("Number is not 10\n");
+}
+
+return 0;
+
+}
+
+
+
