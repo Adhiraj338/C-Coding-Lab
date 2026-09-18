cc6b9990669c487ff641f7e077b2d081e46424a3
Adhiraj_1408 <adhirajramtirthe952@gmail.com>
2026-09-18 13:53:56 +0530

    Add files via upload
    Check character using if-else and NOT operator

---
+++ practice_6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;
+
+    printf("Enter the number:\n");
+    scanf("%d",&number);
+
+    if(number>0){
+        printf("number is positive\n");
+    }
+    else if(number<0){
+        printf("number is negative\n");
+    }
+    else{
+        printf("number is 0\n");
+    }
+
+    return 0;
+}
