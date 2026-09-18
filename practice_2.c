65ae1ddae4160654c31199cd170031fd1fb0b6b0
Adhiraj_1408 <adhirajramtirthe952@gmail.com>
2026-09-18 09:59:17 +0530

    check using if-else and NOT operator

--- practice_2.c
+++ practice_2.c
@@ -7,14 +7,14 @@
         printf("Enter the age :\n");
         scanf("%d",&age);
 
-if(!(age<18)){
-         printf("Age is 18 or older\n");
-}
+        if(!(age<18)){
+          printf("Age is 18 or older\n");
+          }
 
-else{
- printf("Age is under 18\n");
-}
+        else{
+             printf("Age is under 18\n");
+         }
 
-return 0;
+         return 0;
 
-}
+        }
